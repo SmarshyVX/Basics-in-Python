728f1e4dfdf7db3a2e18326c2dc8b6a0114ed6ad
SmarshyVX <rodneyadade687@gmail.com>
2025-12-10 16:34:11 +0000

    first commit

---
+++ exercisee.py
@@ -0,0 +1,15 @@
+playlist = ["4Raws","Timeless","My Heart","Doomsday","No Idea"]#Q1
+
+top_artists=("EsDee","Don Toliver","Odeal","MFDOOM")#Q2
+
+my_music=(playlist,top_artists)#Q3
+
+print(len(my_music))#Q4
+       
+print(playlist[2])#Q5
+
+playlist[0] = "Perfect"#Q6
+
+playlist.append("Blinding Lights")#Q7
+
+print(playlist)#Q8
